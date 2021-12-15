7ca18f63a55b685a04121e2abcb4f60532e76f07
Gozzissimo <fantini.nico@gmail.com>
2021-12-15 16:14:11 +0100

    text-1 con id su h1

--- js/script.js
+++ js/script.js
@@ -8,6 +8,11 @@
 // Inseriamo una immagine e accanto un numero che identifica il numero di prodotti desiderati, i dati devono venire da Vue.
 // Poi aggiungiamo un pulsante per aumentare questo numero.
 
-const app = new Vue(
-    
+var app = new Vue(
+    {
+        el: '#app',
+        data: {
+            message: 'Questo è il mio primo messaggio con Vue'
+        }
+    }
 );
